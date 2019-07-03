ff16312ed846d94168d44140976f99072183685e
Daniel <danielbanasen@gmail.com>
2019-07-03 19:18:36 +0800

    supplies component still unsure of what to make of

--- src/containers/settings/Supply/Supply.js
+++ src/containers/settings/Supply/Supply.js
@@ -18,61 +18,179 @@ const supplies = [
 
 class Supply extends Component {
   state = {
-    supplyForm: {
-      supply: formFunction('select', null, null, [
-        selectOptions('gravel', 'Gravel'),
-        selectOptions('riverMixed', 'River Mixed'),
-        selectOptions('crushedSand', 'Crushed Sand'),
-        selectOptions('riverSand', 'River Sand'),
-        selectOptions('boulder', 'Boulder'),
-        selectOptions('hollowBlocks', 'Hollow Blocks'),
-        selectOptions('cement', 'Cement')
-      ])
+    supplyForm: {},
+    supplyForms: [{ supply: 'gravel' }],
+    // refactor this later
+    suppliesConf: {
+      gravel: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      },
+      riverMixed: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      },
+      crushedSand: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      },
+      riverSand: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      },
+      boulder: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      },
+      hollowBlocks: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      },
+      cement: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      }
     },
-    supplyForms: [{ supply: 'gravel' }]
+    hollowBlocksForm: {
+      fourInches: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      },
+      fiveInches: {
+        elementConfig: {
+          type: 'number',
+          placeholder: '0',
+          disabled: true
+        },
+        value: ''
+      }
+    }
   };
-  onChangeValueHandler = e => {
-    console.log(e);
+  onChangeValueHandler = (index, name, e) => {
+    console.log(index, name);
+    const newSuppConf = JSON.parse(JSON.stringify(this.state.suppliesConf));
+    newSuppConf[name].elementConfig.disabled = !newSuppConf[name].elementConfig
+      .disabled;
+    this.setState({ suppliesConf: newSuppConf });
   };
 
   render() {
     // add an add button that adds the supply to one of the stacks available
     // or add status on them
     let input = [];
-    for (let inputKey in this.state.supplyForm) {
+    let i = 0;
+    for (let supply in this.state.suppliesConf) {
       input.push(
-        <Input
-          key={inputKey}
-          name={inputKey}
-          elementInputType={this.state.supplyForm[inputKey].elementType}
-          elementConfig={this.state.supplyForm[inputKey].elementConfig}
-          change={this.onChangeValueHandler}
-        />
+        <div key={supply} className={styles.supplyRow}>
+          <Input
+            name={supply}
+            nowrap="noWrap"
+            elementInputType="select"
+            elementConfig={{
+              options: [
+                selectOptions('notAvailable', 'N/A'),
+                selectOptions('available', 'Available')
+              ]
+            }}
+            change={this.onChangeValueHandler}
+            ind={i}
+          />
+          <Input
+            name={supply}
+            label="noLabel"
+            elementInputType="input"
+            elementConfig={this.state.suppliesConf[supply].elementConfig}
+            change={this.onChangeValueHandler}
+            ind={i}
+          />
+        </div>
+      );
+      i++;
+    }
+    for (let hollow in this.state.hollowBlocksForm) {
+      input.push(
+        <div key={hollow} className={styles.supplyRow}>
+          <Input
+            name={hollow}
+            nowrap="noWrap"
+            elementInputType="select"
+            elementConfig={{
+              options: [
+                selectOptions('notAvailable', 'N/A'),
+                selectOptions('available', 'Available')
+              ]
+            }}
+            change={this.onChangeValueHandler}
+            ind={i}
+          />
+          <Input
+            name={hollow}
+            label="noLabel"
+            elementInputType="input"
+            elementConfig={this.state.hollowBlocksForm[hollow].elementConfig}
+            change={this.onChangeValueHandler}
+            ind={i}
+          />
+        </div>
       );
     }
-    input = supplies.map(supply => (
-      <div key={supply} className={styles.supplyRow}>
-        <Input
-          name={supply}
-          nowrap="noWrap"
-          elementInputType="select"
-          elementConfig={{
-            options: [
-              selectOptions('available', 'Available'),
-              selectOptions('notAvailable', 'N/A')
-            ]
-          }}
-          change={this.onChangeValueHandler}
-        />
-        <Input
-          name={supply}
-          label="noLabel"
-          elementInputType="input"
-          elementConfig={{ type: 'number', placeholder: '0' }}
-          change={this.onChangeValueHandler}
-        />
-      </div>
-    ));
+    // input = supplies.map((supply, i) => (
+    //   <div key={supply} className={styles.supplyRow}>
+    //     <Input
+    //       name={supply}
+    //       nowrap="noWrap"
+    //       elementInputType="select"
+    //       elementConfig={{
+    //         options: [
+    //           selectOptions('notAvailable', 'N/A'),
+    //           selectOptions('available', 'Available')
+    //         ]
+    //       }}
+    //       change={this.onChangeValueHandler}
+    //       ind={i}
+    //     />
+    //     <Input
+    //       name={supply}
+    //       label="noLabel"
+    //       elementInputType="input"
+    //       elementConfig={{ type: 'number', placeholder: '0', disabled: true }}
+    //       change={this.onChangeValueHandler}
+    //       ind={i}
+    //     />
+    //   </div>
+    // ));
     return input;
   }
 }
