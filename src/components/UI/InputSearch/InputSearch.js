5a585e96e7b1f215cf75d9a67a0125c5a7389823
Daniel <danielbanasen@gmail.com>
2019-07-17 18:37:27 +0800

    split code to be more leaner, made the inputSearch dumb component to class to handle its own autosuggestion

--- src/components/UI/InputSearch/InputSearch.js
+++ src/components/UI/InputSearch/InputSearch.js
@@ -1,21 +1,133 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+import * as actions from '../../../store/actions/index';
 
 import styles from './InputSearch.module.scss';
 
-const inputSearch = props => {
-  return (
-    <div className={styles.search}>
-      <span className={styles.searchIcon}>&#9906;</span>
-      <input
-        className={styles.input}
-        placeholder={props.elementConfig.placeholder}
-        type="text"
-        value={props.value}
-        onChange={props.edit}
-        onKeyDown={props.keydown}
-      />
-    </div>
-  );
-};
+class InputSearch extends Component {
+  state = {
+    inputSupplies: [],
+    filteredSuppliesState: [],
+    focusedItemIndex: '',
+    searchForm: ''
+  };
+  componentDidMount() {
+    this.setState({ inputSupplies: this.props.data });
+  }
 
-export default inputSearch;
+  onKeyPressHandler = e => {
+    let copyFiltered = [...this.state.filteredSuppliesState];
+    if (
+      (e.keyCode === 40 || e.keyCode === 38) &&
+      e.target.value.length > 0 &&
+      copyFiltered.length > 0
+    ) {
+      e.preventDefault();
+      const itemsLength = this.state.filteredSuppliesState.length - 1;
+      let copyFocusedItemIndex = this.state.focusedItemIndex;
+      let currentItemIndex;
+      if (
+        itemsLength === -1 ||
+        this.state.searchForm.length === 0 ||
+        e.target.value.length <= 0
+      )
+        copyFocusedItemIndex = 0;
+      currentItemIndex = copyFocusedItemIndex === '' ? 0 : copyFocusedItemIndex;
+      if (e.keyCode === 40) {
+        if (
+          copyFocusedItemIndex === itemsLength ||
+          copyFocusedItemIndex > itemsLength ||
+          copyFocusedItemIndex === ''
+        ) {
+          currentItemIndex = 0;
+        } else {
+          currentItemIndex++;
+        }
+      } else if (e.keyCode === 38) {
+        if (currentItemIndex === 0 || copyFocusedItemIndex === 0) {
+          currentItemIndex = itemsLength;
+        } else {
+          currentItemIndex = currentItemIndex-- === -1 ? 0 : currentItemIndex;
+        }
+      }
+      let value = e.target.value;
+      if (this.state.filteredSuppliesState.length !== -1) {
+        value = copyFiltered[currentItemIndex].materials;
+      }
+
+      if (
+        this.state.searchForm.length > 0 ||
+        this.state.copyFocusedItemIndex !== -1
+      ) {
+        this.setState({
+          focusedItemIndex: currentItemIndex,
+          searchForm: value
+        });
+      }
+    }
+    if (e.keyCode === 13) {
+      // console.log(
+      //   this.state.filteredSuppliesState[this.state.focusedItemIndex]
+      // );
+      this.props.onAddItemsToBuyDispatch(
+        this.state.filteredSuppliesState[this.state.focusedItemIndex]
+      );
+    }
+  };
+
+  onSearchFormhandler = e => {
+    this.setState({
+      searchForm: e.target.value,
+      filteredSuppliesState: this.state.inputSupplies.filter(material =>
+        material.materials.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    });
+  };
+
+  render() {
+    const { props } = this;
+    let suggestion =
+      this.state.searchForm.length > 0 ? (
+        <div className={styles.autosuggest}>
+          {' '}
+          {this.state.filteredSuppliesState.map((supply, i) => (
+            <div
+              key={supply.materials}
+              className={[
+                styles.suggestItem,
+                styles[i === this.state.focusedItemIndex ? 'isActive' : null]
+              ].join(' ')}
+              // isActive={i === this.state.focusedItemIndex}
+            >
+              {supply.materials}
+            </div>
+          ))}
+        </div>
+      ) : null;
+    return (
+      <div className={styles.search}>
+        <span className={styles.searchIcon}>&#9906;</span>
+        <input
+          className={styles.input}
+          placeholder={props.elementConfig.placeholder}
+          type="text"
+          value={this.state.searchForm}
+          onChange={this.onSearchFormhandler}
+          onKeyDown={this.onKeyPressHandler}
+        />
+        {suggestion}
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  onAddItemsToBuyDispatch: item => dispatch(actions.addItemsToSales(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(InputSearch);
--- src/containers/Invoice/Invoice.js
+++ src/containers/Invoice/Invoice.js
@@ -4,11 +4,10 @@ import styles from './Invoice.module.scss';
 
 import { connect } from 'react-redux';
 
-import * as actions from '../../store/actions/index';
+// import * as actions from '../../store/actions/index';
 
 import Head from '../../components/UI/Head/Head';
 import HeadChild from '../../components/UI/HeadChild/HeadChild';
-import Input from '../../components/UI/Input/Input';
 import POSTable from '../../components/POSTable/POSTable';
 import InputSearch from '../../components/UI/InputSearch/InputSearch';
 import POSCustomer from '../../components/POSCustomer/POSCustomer';
@@ -36,110 +35,8 @@ class Invoice extends Component {
   onFormChangeHandler = (_, name, e) => {
     console.log(name, e.target.value);
   };
-  onSearchFormhandler = e => {
-    this.setState({
-      searchForm: e.target.value,
-      filteredSuppliesState: this.state.suppliesState.filter(material =>
-        material.materials.toLowerCase().includes(e.target.value.toLowerCase())
-      )
-    });
-  };
-  onKeyPressHandler = e => {
-    let copyFiltered = [...this.state.filteredSuppliesState];
-    if (
-      (e.keyCode === 40 || e.keyCode === 38) &&
-      e.target.value.length > 0 &&
-      copyFiltered.length > 0
-    ) {
-      e.preventDefault();
-      const itemsLength = this.state.filteredSuppliesState.length - 1;
-      let copyFocusedItemIndex = this.state.focusedItemIndex;
-      let currentItemIndex;
-      if (
-        itemsLength === -1 ||
-        this.state.searchForm.length === 0 ||
-        e.target.value.length <= 0
-      )
-        copyFocusedItemIndex = 0;
-      currentItemIndex = copyFocusedItemIndex === '' ? 0 : copyFocusedItemIndex;
-      if (e.keyCode === 40) {
-        if (
-          copyFocusedItemIndex === itemsLength ||
-          copyFocusedItemIndex > itemsLength ||
-          copyFocusedItemIndex === ''
-        ) {
-          currentItemIndex = 0;
-        } else {
-          currentItemIndex++;
-        }
-      } else if (e.keyCode === 38) {
-        if (currentItemIndex === 0 || copyFocusedItemIndex === 0) {
-          currentItemIndex = itemsLength;
-        } else {
-          currentItemIndex = currentItemIndex-- === -1 ? 0 : currentItemIndex;
-        }
-      }
-      let value = e.target.value;
-      if (this.state.filteredSuppliesState.length !== -1) {
-        value = copyFiltered[currentItemIndex].materials;
-      }
-
-      if (
-        this.state.searchForm.length > 0 ||
-        this.state.copyFocusedItemIndex !== -1
-      ) {
-        this.setState({
-          focusedItemIndex: currentItemIndex,
-          searchForm: value
-        });
-      }
-    }
-    if (e.keyCode === 13) {
-      console.log(
-        this.state.filteredSuppliesState[this.state.focusedItemIndex]
-      );
-      this.props.onAddItemsToBuyDispatch(
-        this.state.filteredSuppliesState[this.state.focusedItemIndex]
-      );
-    }
-  };
 
   render() {
-    let inputForm = Object.keys(this.state.invoiceForm)
-      .filter(key => key !== 'item')
-      .map(keys => {
-        return (
-          <div key={keys}>
-            <Input
-              name={keys}
-              elementInputType="input"
-              elementConfig={this.state.invoiceForm[keys]}
-              change={this.onFormChangeHandler}
-              ind={0}
-              color="blue"
-            />
-          </div>
-        );
-      });
-
-    let suggestion =
-      this.state.searchForm.length > 0 ? (
-        <div className={styles.autosuggest}>
-          {' '}
-          {this.state.filteredSuppliesState.map((supply, i) => (
-            <div
-              key={supply.materials}
-              className={[
-                styles.suggestItem,
-                styles[i === this.state.focusedItemIndex ? 'isActive' : null]
-              ].join(' ')}
-              // isActive={i === this.state.focusedItemIndex}
-            >
-              {supply.materials}
-            </div>
-          ))}
-        </div>
-      ) : null;
     return (
       <div className={styles.invoiceMain}>
         <Head classname="blue" svgname="invoice">
@@ -151,11 +48,8 @@ class Invoice extends Component {
               <InputSearch elementConfig={{ placeholder: 'Customer' }} />
               <InputSearch
                 elementConfig={{ placeholder: 'Item' }}
-                value={this.state.searchForm}
-                edit={this.onSearchFormhandler}
-                keydown={this.onKeyPressHandler}
+                data={this.props.supplies}
               />
-              {suggestion}
             </div>
             <POSCustomer />
             <div className={styles.summaryWrapper}>
@@ -183,21 +77,6 @@ class Invoice extends Component {
           </div>
           <div className={styles.invoiceForm}>Invoice Form</div>
         </div>
-        <div>
-          <form className={styles.form}>
-            <div className={styles.item}>
-              <Input
-                name="Item"
-                elementInputType="input"
-                elementConfig={this.state.invoiceForm.item}
-                change={this.onFormChangeHandler}
-                ind={0}
-                color="blue"
-              />
-            </div>
-            {inputForm}
-          </form>
-        </div>
       </div>
     );
   }
@@ -207,11 +86,5 @@ const mapStateToProps = state => ({
   supplies: state.supplySettings.activeSupplies,
   itemsToBuy: state.invoicePOS.itemsToBuy
 });
-const mapDispatchToProps = dispatch => ({
-  onAddItemsToBuyDispatch: item => dispatch(actions.addItemsToSales(item))
-});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Invoice);
+export default connect(mapStateToProps)(Invoice);
