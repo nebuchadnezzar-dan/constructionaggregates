571657f9fbab450a6d1f87ffc68d5080dd717522
Daniel <danielbanasen@gmail.com>
2019-10-07 17:02:24 +0800

    initial customer ind view design

--- src/containers/Customers/Customer/Customer.js
+++ src/containers/Customers/Customer/Customer.js
@@ -6,17 +6,44 @@ import * as actions from '../../../store/actions/index';
 
 import styles from './Customer.module.scss';
 
+import Button from '../../../components/UI/Button/Button';
+
 class Customer extends Component {
 
-    componentDidMount() {
-        // console.log(+this.props.location.pathname.match(/\d+/)[0]);
-        // this.props.fetchCustomer(+this.props.location.pathname.match(/\d+/)[0]);
-
-    }
 
     render() {
-        console.log(this.props.data);
-        return <div>Customer</div>
+        const { id, lastName, firstName, contactNo } = this.props.data;
+        return <div>
+            <div className={styles.customerWrapper}>
+                <div className={styles.title}>{id} | {`${lastName}, ${firstName}`} </div>
+                <div className={styles.body}>
+                    <div>
+                        <div className={styles.label}>Last Name</div>
+                        <div className={styles.value}>{lastName}</div>
+                    </div>
+                    <div>
+                        <div className={styles.label}>First Name</div>
+                        <div className={styles.value}>{firstName}</div>
+                    </div>
+                    <div>
+                        <div className={styles.label}>Contact No.</div>
+                        <div className={styles.value}>{contactNo}</div>
+                    </div>
+
+                    <hr className={styles.line} />
+
+                    <div>
+                        <Button color="blue">Edit</Button>
+                        <Button color="red">Delete</Button>
+                        <Button color="orange">Credits</Button>
+                        <Button color="violet">Purchase History</Button>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
     }
 
 };
