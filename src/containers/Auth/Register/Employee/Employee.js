e6c8064439b75956fc8fecf97ccd50c99a674cd9
Daniel <danielbanasen@gmail.com>
2019-12-10 22:09:12 +0800

    auth page css animation tinkering

--- src/containers/Auth/Register/Employee/Employee.js
+++ src/containers/Auth/Register/Employee/Employee.js
@@ -31,51 +31,85 @@ class Employee extends Component {
             <div className={styles.out}>
                 <div className={styles.circle} />
                 <div className={styles.triangle} />
-                <div className={styles.innerBody}>
-                    <div className={this.state.default ? styles.login : styles.regis}>
-                        <div className={styles.logo}>Logo</div>
-                        <div className={styles.loginCenter}>
-                            <div>Welcome Back!</div>
-                            <div>Login to start and continue your work.</div>
-                            <div><Button register="regular" click={this.changeView}>SIGN IN</Button></div>
+                <div className={[styles.innerBody, this.state.default ? null : styles.leftPanelActive].join(' ')}>
+                    <div className={styles.welcomeContainer}>
+                        <div className={styles.welcome}>
+                            <div className={[styles.welcomePanel, styles.welcomeLeft].join(' ')}>
+                                <div className={styles.logo}>Logo</div>
+                                <div className={styles.loginCenter}>
+                                    <div>Welcome Back!</div>
+                                    <div>Login to start and continue your work.</div>
+                                    <div><Button register="regular" click={this.changeView}>SIGN IN</Button></div>
+                                </div>
+                            </div>
+                            <div className={[styles.welcomePanel, styles.welcomeRight].join(' ')}>
+                                <div className={styles.logo}>Logo</div>
+                                <div className={styles.loginCenter}>
+                                    <div>Hello Friend!</div>
+                                    <div>Enter your personal details and start your journey with us</div>
+                                    <div><Button register="regular" click={this.changeView}>SIGN UP</Button></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className={this.state.default ? styles.register : styles.logs}>
-                        <div>
-                            <div className={styles.regTitle}>Create An Account</div>
-                            <div className={styles.buttonWrap}>
-                                <Button register="icons">&#120307;</Button>
-                                <Button register="icons">G+</Button>
-                                <Button register="icons">in</Button>
-                            </div>
-                            <div className={styles.registerField}>
-                                <form className={styles.form}
-                                    onSubmit={this.submitHandler}>
-                                    <div className={styles.field}>
-                                        <span><User /></span>
-                                        <input type="text" placeholder="Last Name" />
-                                    </div>
-                                    <div className={styles.field}>
-                                        <span><User1 /></span>
-                                        <input type="text" placeholder="First Name" />
-                                    </div>
-                                    <div className={styles.field}>
-                                        <span><Email /></span>
-                                        <input type="email" placeholder="Email" />
-                                    </div>
-                                    <div className={styles.field}>
-                                        <span><Contact /></span>
-                                        <input type="text" placeholder="Contact No." />
-                                    </div>
-                                    <div className={styles.field}>
-                                        <span><Lock /></span>
-                                        <input type="password" placeholder="Password" />
-                                    </div>
-                                    <div className={styles.submit}>
-                                        <Button register="reg">Sign Up</Button>
-                                    </div>
-                                </form>
-                            </div>
+                    <div className={[styles.formContainer, styles.SignUpContainer].join(' ')}>
+                        <div className={styles.regTitle}>Create An Account</div>
+                        <div className={styles.buttonWrap}>
+                            <Button register="icons">&#120307;</Button>
+                            <Button register="icons">G+</Button>
+                            <Button register="icons">in</Button>
+                        </div>
+                        <div className={styles.registerField}>
+                            <form className={styles.form}
+                                onSubmit={this.submitHandler}>
+                                <div className={styles.field}>
+                                    <span><User /></span>
+                                    <input type="text" placeholder="Last Name" />
+                                </div>
+                                <div className={styles.field}>
+                                    <span><User1 /></span>
+                                    <input type="text" placeholder="First Name" />
+                                </div>
+                                <div className={styles.field}>
+                                    <span><Email /></span>
+                                    <input type="email" placeholder="Email" />
+                                </div>
+                                <div className={styles.field}>
+                                    <span><Contact /></span>
+                                    <input type="text" placeholder="Contact No." />
+                                </div>
+                                <div className={styles.field}>
+                                    <span><Lock /></span>
+                                    <input type="password" placeholder="Password" />
+                                </div>
+                                <div className={styles.submit}>
+                                    <Button register="reg">Sign Up</Button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className={[styles.formContainer, styles.SignInContainer].join(' ')}>
+                        <div className={styles.regTitle}>Sign In</div>
+                        <div className={styles.buttonWrap}>
+                            <Button register="icons">&#120307;</Button>
+                            <Button register="icons">G+</Button>
+                            <Button register="icons">in</Button>
+                        </div>
+                        <div className={styles.registerField}>
+                            <form className={styles.form}
+                                onSubmit={this.submitHandler}>
+                                <div className={styles.field}>
+                                    <span><Email /></span>
+                                    <input type="email" placeholder="Email" />
+                                </div>
+                                <div className={styles.field}>
+                                    <span><Lock /></span>
+                                    <input type="password" placeholder="Password" />
+                                </div>
+                                <div className={styles.submit}>
+                                    <Button register="reg">Sign In</Button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
